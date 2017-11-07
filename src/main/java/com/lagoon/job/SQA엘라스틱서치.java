5048f3026510e74bc00238f7704bc06ff0b40e0d
Kang MyungJung <mj.kang@piolink.com>
2017-11-07 15:16:23 +0900

    shoud, mustNot 테스트

--- src/main/java/com/lagoon/job/SQA엘라스틱서치.java
+++ src/main/java/com/lagoon/job/SQA엘라스틱서치.java
@@ -1,15 +1,18 @@
 package com.lagoon.job;
 
 import java.util.Calendar;
+import java.util.Date;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.Aggregations;
 import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Scope;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.core.ResultsExtractor;
@@ -17,6 +20,7 @@ import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilde
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
 
+import com.lagoon.LagoonApplication;
 import com.lagoon.utils.DateUtils;
 
 @Scope("prototype")
@@ -50,9 +54,10 @@ public class SQA엘라스틱서치 {
         BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery();
         queryBuilder.must(QueryBuilders.rangeQuery("time").gte(from).lte(to));
         queryBuilder.must(QueryBuilders.termQuery("state", "create"));
-        //srcIpMac 필드 Null 제외
-        queryBuilder.mustNot(QueryBuilders.termQuery("srcIp", ""));
-//        queryBuilder.mustNot(QueryBuilders.termQuery("srcMac", ""));
+        //(srcIp = '' or srcMac = '')
+        queryBuilder.should(QueryBuilders.boolQuery().mustNot(QueryBuilders.termQuery("srcIp", "")));
+        queryBuilder.should(QueryBuilders.boolQuery().mustNot(QueryBuilders.termQuery("srcMac", "")));
+        queryBuilder.minimumShouldMatch("1");
         
         SearchQuery searchQuery = new NativeSearchQueryBuilder()
         .withIndices(event_logs_index).withTypes(timatrix_log_type)
@@ -72,7 +77,37 @@ public class SQA엘라스틱서치 {
         for (Terms.Bucket bucket : terms.getBuckets()){
             // ${srcIp}/${srcMac}-${portId}+${cswitchId}
             String srcIpMac = bucket.getKey();
-            System.out.println(srcIpMac);
+            String srcIp = srcIpMac.substring(0, srcIpMac.indexOf("/"));
+            String srcMac = srcIpMac.substring(srcIpMac.indexOf("/") + 1, srcIpMac.indexOf("-"));
+            long docCount = bucket.getDocCount();
+            if (srcIp.equals("") && srcMac.equals("")) {
+                System.out.println("#1. SRC IP : " + srcIp + ", SRC MAC : " + srcMac + ", COUNT : " + docCount);
+            } else if (srcIp.equals("") && !srcMac.equals("")) {
+                System.out.println("##2. SRC IP : " + srcIp + ", SRC MAC : " + srcMac + ", COUNT : " + docCount);
+            } else if (!srcIp.equals("") && srcMac.equals("")) {
+                System.out.println("###3. SRC IP : " + srcIp + ", SRC MAC : " + srcMac + ", COUNT : " + docCount);
+            } else {
+                System.out.println("####4. SRC IP : " + srcIp + ", SRC MAC : " + srcMac + ", COUNT : " + docCount);
+            }
         }
+    }
+    
+    public static void main(String[] args) {
+
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.add(Calendar.DATE, -7);
+        startCalendar.set(Calendar.HOUR_OF_DAY, 00);
+        startCalendar.set(Calendar.MINUTE, 00);
+        startCalendar.set(Calendar.SECOND, 0);
+          
+        Calendar endCalendar = Calendar.getInstance();
+        endCalendar.set(Calendar.HOUR_OF_DAY, 23);
+        endCalendar.set(Calendar.MINUTE, 59);
+        endCalendar.set(Calendar.SECOND, 59);
+        Long from = startCalendar.getTimeInMillis(); 
+        Long to = endCalendar.getTimeInMillis() - 1;
+        System.out.println(DateUtils.getFormatString("yyyy-MM-dd HH:mm:ss.SSS", new Date(from)));
+        System.out.println(DateUtils.getFormatString("yyyy-MM-dd HH:mm:ss.SSS", new Date(to)));
+        
     }
 }
